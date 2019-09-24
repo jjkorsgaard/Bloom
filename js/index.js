ef730074328b383a151854f89e471316117c6f0f
jjkorsgaard <jjkorsgaard@gmail.com>
2019-09-24 11:51:15 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -73,7 +73,7 @@ function conn(){
 	bleDeviceName = deviceTouchArr[0];
 	document.getElementById("debugDiv").innerHTML += "<br>Debug: <br>"+deviceTouchArr[0]+"<br>"+deviceTouchArr[1]; //for debug:
 	if(bleDeviceName  == "WHATEVER")
-		test();
+		showCleaning();
  }
  
 
