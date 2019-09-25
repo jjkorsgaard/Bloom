348fc34199211caf44a7ce5cbcb8a178ecac4c64
jjkorsgaard <jjkorsgaard@gmail.com>
2019-09-25 09:34:30 +0200

    update

--- js/index.js
+++ js/index.js
@@ -94,8 +94,6 @@ function onConnError(){
 
 function data(txt){
 	messageInput.value = txt;
-	var data = stringToBytes(messageInput.value);
-	ble.writeWithoutResponse(ConnDeviceId, blue.serviceUUID, blue.txCharacteristic, data, onSend, onError);
 }	
 
 function sendData() { // send data to Arduino
